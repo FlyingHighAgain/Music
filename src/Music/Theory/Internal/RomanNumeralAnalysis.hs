d126090b9dfe44fbacb644dc65564c5ee72b4638
FlyingHighAgain <tdnr.sym@gmail.com>
2015-02-20 00:29:55 +0900

    upgrade analyzeChordProgression function

--- src/Music/Theory/Internal/RomanNumeralAnalysis.hs
+++ src/Music/Theory/Internal/RomanNumeralAnalysis.hs
@@ -3,6 +3,9 @@ module Music.Theory.Internal.RomanNumeralAnalysis(analyzeChordProgression) where
 import Music.Theory.Internal.Degree
 import Music.Theory.Internal.Chord
 
+import Data.String.Utils(replace, split, splitWs)
+import Data.List(nub)
+
 
 toRomanNumeral :: String -> String -> String
 toRomanNumeral root chord = degree ++ symbol
@@ -11,4 +14,20 @@ toRomanNumeral root chord = degree ++ symbol
     symbol    = getSymbol chord
     degree    = getDegree root chordRoot
 
-analyzeChordProgression root progression = unwords $ map (toRomanNumeral root) $ words progression
+--analyzeChordProgression root progression = unwords $ map (toRomanNumeral root) $ words progression
+
+removeBars :: String -> String
+removeBars = replace "|" " "
+
+splitChordElements :: String -> [String]
+splitChordElements = concat . map (split "/") . splitWs . removeBars
+
+getChordElements :: String -> [String]
+getChordElements = nub . splitChordElements
+
+analyzeChordProgression :: String -> String -> String
+analyzeChordProgression root progression = foldr (\(old, new) -> replace old new) progression (zip oldList newList)
+  where
+  	root'   = getRootNote root 
+  	oldList = getChordElements progression
+  	newList = map (toRomanNumeral root') oldList
